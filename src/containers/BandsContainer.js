4d70d9388fbac28a0c08074eff37d69326358e2f
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-11 15:47:06 -0500

    commit tree c236503bbde9daaa1b6d9c420089e1f9d6a02e94

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import BandInput from "../components/BandInput";
 
 class BandsContainer extends Component {
   render() {
-    return(
+    return (
       <div>
-        BandsContainer
+        <ul>
+          {this.props.bands.map((band) => (
+            <li key={band.name}>{band.name}</li>
+          ))}
+        </ul>
+        <BandInput addBand={this.props.manageBands} />
       </div>
-    )
+    );
   }
 }
+const mapStateToProps = (state) => {
+  return state;
+};
 
-export default BandsContainer
+const mapDispatchToProps = (dispatch) => ({
+  manageBands: (band) => dispatch({ type: "ADD_BAND", band }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer);
